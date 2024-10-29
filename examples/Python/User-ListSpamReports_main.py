6862ef4a6b95590620eb93c6edfac314691e0975
DSikes <david@sikespc.com>
2024-10-29 13:13:47 -0400

    Add two more examples

---
+++ examples/Python/User-ListSpamReports_main.py
@@ -0,0 +1,75 @@
+﻿from datetime import datetime
+
+import aiohttp
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+# Please note that this basic example won't work if you have an asyncio event loop running.
+# In some python environments (as with Jupyter which uses IPython) an asyncio event loop is created for you.
+# In that case you should use instead https://gql.readthedocs.io/en/latest/async/async_usage.html#async-usage
+def user_list_spam_reports(bearer_token, channel_type = None, issue_type = None, included_status = None, excluded_status = None, limit_count = 10):
+    transport = AIOHTTPTransport(
+        url="https://api.staging.v2.tnid.com/user",
+        headers=
+        {
+            "Authorization": f"Bearer {bearer_token}"
+        }
+    )
+
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    query = gql(
+            """
+                query (
+                    $channelType: SpamReportChannelType
+                    $issueType: SpamReportIssueType
+                    $includedStatus: SpamReportStatus
+                    $excludedStatus: SpamReportStatus
+                    $limit: Int
+                  ) {
+                    spamReports (
+                    channelType: $channelType
+                    issueType: $issueType
+                    includedStatus: $includedStatus
+                    excludedStatus: $excludedStatus
+                    limit: $limit
+                    ) {
+                    id
+                    fromNumber
+                    toNumber
+                    userNote
+                    messageContent
+                    channelType
+                    issueType
+                    status
+                    createdAt
+                    updatedAt
+                    timestamp
+                    metadata
+                    user {
+                        id
+                    }
+                    }
+                  }
+            """
+    )
+
+    params = { "channelType": channel_type,
+               "issueType": issue_type,
+               "includedStatus": included_status,
+               "excludedStatus": excluded_status,
+               "limit": limit_count }
+
+    try:
+        # Execute the query on the transport
+        response = client.execute(query, params)
+        print(f"Response OK: {response}")
+        return response
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+# Example usage:
+token = "your_user_token"
+user_list_spam_reports(token, "MMS")
---
+++ examples/Python/User-UpdateUserProfile_main.py
@@ -0,0 +1,74 @@
+﻿from datetime import datetime
+
+import aiohttp
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+# Please note that this basic example won't work if you have an asyncio event loop running.
+# In some python environments (as with Jupyter which uses IPython) an asyncio event loop is created for you.
+# In that case you should use instead https://gql.readthedocs.io/en/latest/async/async_usage.html#async-usage
+def user_update_profile(bearer_token, username = None, first_name = None, last_name = None, middle_name = None, birthdate = None, about_me = None, metadata = None):
+    transport = AIOHTTPTransport(
+        url="https://api.staging.v2.tnid.com/user",
+        headers=
+        {
+            "Authorization": f"Bearer {bearer_token}"
+        }
+    )
+
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    query = gql(
+            """
+             mutation (
+                $username: String
+                $firstName: String
+                $lastName: String
+                $middleName: String
+                $birthdate: Date
+                $aboutMe: String
+                $metadata: Json
+              ) {
+                updateUser (
+                username: $username
+                firstName: $firstName
+                lastName: $lastName
+                middleName: $middleName
+                birthdate: $birthdate
+                aboutMe: $aboutMe
+                metadata: $metadata
+                ) {
+                id
+                username
+                firstName
+                lastName
+                middleName
+                birthdate
+                aboutMe
+                metadata
+                }
+              }
+            """
+    )
+
+    params = { "username": username,
+               "firstName": first_name,
+               "lastName": last_name,
+               "middleName": middle_name,
+               "birthdate": birthdate,
+               "aboutMe": about_me,
+               "metadata": metadata }
+
+    try:
+        # Execute the query on the transport
+        response = client.execute(query, params)
+        print(f"Response OK: {response}")
+        return response
+    except Exception as e:
+        print(f"Exception: {e}")
+
+
+# Example usage:
+token = "your_user_token"
+user_update_profile(token, "username", "John", "Smith")
